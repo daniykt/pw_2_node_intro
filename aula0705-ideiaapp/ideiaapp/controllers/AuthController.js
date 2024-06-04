c3529586d4ace0215b6281f5e42f0728fc19435a
daniykt <daniykt7@gmail.com>
2024-06-04 20:36:26 -0300

    Aula 0406 estruração de projeto para deploy e conector azure

--- aula0705-ideiaapp/ideiaapp/controllers/AuthController.js
+++
@@ -1,9 +0,0 @@
-const User = require('../models/User')
-const bcrypt = require('bcryptjs')
-
-modulo.exports = class UserController {
-    static login(req, res) {
-        res.render('auth/login')
-    }
-    
-}
